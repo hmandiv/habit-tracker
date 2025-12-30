7c107a24a09badcb34cad445e72693d569aeeba6
HRMandiv <mandivcei@gmail.com>
2025-12-30 03:41:37 -0500

    refactor: convert isDoneOnDay to arrow function

--- src/features/habits/model/selectors.ts
+++ src/features/habits/model/selectors.ts
@@ -1,6 +1,6 @@
 import { Day } from "./checkins";
 
-export function isDoneOnDay(days: Day[] | undefined, day: Day): boolean {
+export const isDoneOnDay = (days: Day[] | undefined, day: Day): boolean => {
   if (!days) return false;
   return days.includes(day);
-}
+};
